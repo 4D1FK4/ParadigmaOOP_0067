4d11fd2aac8088becf563c732799c667267d167e
4D1FK4 <adifkapersonal0812@gmail.com>
2026-04-21 18:35:56 +0700

    Adding Public behavior

--- IntroductionOOP.cpp
+++ IntroductionOOP.cpp
@@ -13,3 +13,10 @@ class Mahasiswa{ //blue print atau cetakan
         cout << "Nilai : " << nilai << endl;
     }
 }; // batas class
+
+int main(){
+    Mahasiswa mhs;
+    mhs.nim = 2022;
+    mhs.nama = "SiImut";
+    mhs.nilai = 90.5;
+}
